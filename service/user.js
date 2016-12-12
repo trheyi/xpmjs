aa99f9175742fec060205a8f7bf107076ce59916
Weiping <weiping@diancloud.com>
2016-12-12 13:28:14 +0800

    + Promise

--- service/user.js
+++ service/user.js
@@ -1,27 +1,94 @@
 require('../lib/promise-7.0.4.min.js');
 var Excp = require('excp.js');
 
-function login( name, mobile ) {
-	console.log( name, mobile, Promise );
+function User( option )  {
 
-	// return new Promise(function (resolve, reject) {
-	// 	wx.request({
-	// 		url:'https://wxcloud.tuanduimao.cn/baas/default/index',
+	
+	option = option || {};
+
+	this.host = option['host'] || 'https://wxcloud.tuanduimao.cn/baas/user';
+
+
+	// 用户登录
+	this.login = function() {
+		
+		var that = this;
+		return new Promise(function (resolve, reject) {
+
+			wx.login({
+				success: function(res) {
+
+      				wx.request({
+						url: that.host + '/login',
+						data: { code:res.code},
+						header: {'content-type': 'application/json'},
+						success: function (res){
+							if ( res.statusCode != 200 ) {
+								reject(new Excp('用户登录失败 API错误',500, {
+									'res':res,
+								}));
+								return;
+							}
+
+							if ( typeof res['data'] != 'object') {
+								reject(new Excp('用户登录失败 API错误',500, {
+									'res':res,
+								}));
+							}
+
+							resolve( res['data'] ); 
+						},
+
+						fail: function (res) { 
+
+							reject(new Excp('用户登录失败',500, {
+								'res':res,
+							}));
+						}
+					});
+
+      			},
+
+      			fail: function( res ) {
+      				reject(new Excp('用户登录失败',500, {
+						'res':res,
+					}));
+      			}
+			});
+
+
 			
-	// 		success: function (res){ 
-	// 			resolve( res['data'] ); 
-	// 		},
+		});
+	}
 
-	// 		fail: function (err) { 
-	// 			reject( new Excp(err.errMsg) );
-	// 		}
-	// 	});
-	// });
 
+	this.getUserInfo = function() {
+		
+		return new Promise(function (resolve, reject) {
+			
+			wx.getUserInfo({
+      			success: function(res) {
+
+      				var user = JSON.parse( res.rawData );
+      					user['signature'] = res.signature;
+      					user['iv'] = res.iv;
+      					user['encryptedData'] = res.encryptedData;
+
+      				resolve( user );
+      			},
+
+      			fail: function( res ) {
+      				reject(new Excp('读取用户信息错误',500, {
+						'res':res,
+					}));
+      			}
+      		});
+
+		});
+	}
 }
 
 
 
-module.exports = {
-  login: login
-}
+
+module.exports = User;
--- service/wss.js
+++ service/wss.js
@@ -1,20 +1,52 @@
-var Promise = require('../lib/bluebird.min.js');
+require('../lib/promise-7.0.4.min.js');
 var Excp = require('excp.js');
 
 function Wss( host ) {
+
+	this.isOpen = false;
+	this.events = {};
 
 	this.host = host || 'xxx';
 	
 	this.bind = function( command, cb ) {
 
-		wx.onSocketMessage(function( res ){
-			cb( res );
-		});
-
-		console.log( 'bind command:',  command, ' cb:', cb );
+		this.events[command] = cb;
 	}
 
-	this.send = function ( command, params ) {
+	this.send = function ( command, params, to ) {
+
+		
+		var that = this;
+
+		return new Promise(function (resolve, reject) {
+			
+			if (that.isOpen !== true ) {
+				reject(new Excp('WebSocket未连接,请检查',500,
+				{
+					'command':command,
+					'params': params
+				}));
+			}
+
+			var to = to || null;
+
+			wx.sendSocketMessage({
+		      data:JSON.stringify({'c':command, 'b':params, 't':to }),
+		      success: function( res ){
+		      		resolve( res );
+		      },
+		      fail: function( res ){
+		      	reject(new Excp('消息发送失败',500,
+				{
+					'command':command,
+					'params': params,
+					'res':res
+				}));
+		      }
+		    });
+
+		});
+
 		console.log( 'send command: ', command, ' params:', params );
 	}
 
@@ -24,14 +56,19 @@ function Wss( host ) {
 
 		return new Promise(function (resolve, reject) {
 			wx.connectSocket({
+				header:{ 
+				    'content-type': 'application/json'
+				},
 		  		url: 'wss://' +  that.host + api
 			});
 
 			wx.onSocketOpen(function(res) {
+				that.isOpen = true;
 		  		resolve( res );
 			});
 
 			wx.onSocketError(function(res){
+			  that.isOpen = false;
 			  reject( new Excp(
 			  	'WebSocket连接打开失败，请检查！', 500, 
 			  	{
@@ -41,6 +78,35 @@ function Wss( host ) {
 			  	}
 			  ));
 			})
+
+			wx.onSocketMessage(function( res ){
+
+				if ( typeof res.data !== 'string' ) {
+					return;
+				}
+
+				var resp = JSON.parse( res.data );
+					resp['data'] = resp['data'] || {};
+
+				var code = resp['code'];
+				var req =  resp['data']['request'] || {};
+				var resp = resp['data']['response'] || {};
+				var cmd = req['c'] || null;
+				
+
+				if ( code !== 0  ) {
+					if ( typeof that.events[cmd] == 'function' ) {
+						that.events[cmd]( {'request':req, 'response':resp}, 'error');
+					}
+					return;
+				}
+
+				if ( typeof that.events[cmd] == 'function' ) {
+					that.events[cmd]( {'request':req, 'response':resp}, 'success');
+				}
+
+			});
+
 		});
 	}
 
