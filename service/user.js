8d1653b85b1339a26d751dcabc1143a3e4bbf4b9
Weiping <weiping@diancloud.com>
2016-12-28 20:42:29 +0800

    + 用户退出

--- service/user.js
+++ service/user.js
@@ -16,6 +16,63 @@ function User( option )  {
 	this.tab = new Table( option, this.table_name );
 
 
+	// 用户退出
+	this.logout = function() {
+		
+		var that = this;
+		return new Promise(function (resolve, reject) {
+
+			var reqData = {
+				_sid:that.ss.id(), 
+				_table:that.table_name,
+				_prefix:that.prefix,
+			};
+
+			wx.request({
+				url: that.api + '/logout',
+				data: reqData, // 使用 Code 换取 Session ID 
+				header: {'content-type': 'application/json'},
+				method: 'POST',
+				success: function (res){
+
+					if ( res.statusCode != 200 ) {
+						reject(new Excp('用户退出失败 API错误',500, {
+							'res':res,
+						}));
+						return;
+					}
+
+					if ( typeof res['data'] != 'object') {
+						reject(new Excp('用户退出失败 API错误',500, {
+							'res':res,
+						}));
+						return;
+					}
+
+					if ( res['data']['code'] != 0) {
+						reject(new Excp('用户退出失败, Session 校验失败',500, {
+							'res':res,
+						}));
+						return;
+					}
+					that.ss.destory(); // 清空会话数据
+					resolve( res['data'] );
+				},
+
+				fail: function (res) { 
+
+					reject(new Excp('用户退出失败',500, {
+						'res':res,
+					}));
+				}
+			});
+
+
+		});
+
+	}
+
+
 	// 用户登录
 	this.login = function() {
 		
