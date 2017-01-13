e51121b9a8646334e740bcb051e98d68d07e140e
trheyi <zhuce@diancloud.com>
2017-01-13 18:40:40 +0800

    Utils +merge  合并多个 Object
    ps: object.assign  不兼容

--- service/utils.js
+++ service/utils.js
@@ -164,6 +164,40 @@ function Utils( option ) {
 		
 		return seconds;
 	}
+
+
+	/**
+	 * 合并多个Object 
+	 * @param  Object  n1, n2...  待合并的 Object
+	 * @return Object 
+	 */
+	this.merge = function() {
+
+	    var to = new Object();
+
+	    // 遍历剩余所有参数
+	    for (var i = 0; i < arguments.length; i++) {
+	      var nextSource = arguments[i];
+	      // 参数为空，则跳过，继续下一个
+	      if (nextSource === undefined || nextSource === null) {
+	        continue;
+	      }
+	      nextSource = Object(nextSource);
+
+	      // 获取改参数的所有key值，并遍历
+	      var keysArray = Object.keys(nextSource);
+	      for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+	        var nextKey = keysArray[nextIndex];
+	        var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+	        // 如果不为空且可枚举，则直接浅拷贝赋值
+	        if (desc !== undefined && desc.enumerable) {
+	          to[nextKey] = nextSource[nextKey];
+	        }
+	      }
+	    }
+
+	    return to;
+	}
 }
 
 module.exports = Utils;
